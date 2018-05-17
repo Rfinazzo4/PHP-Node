d36b3fed1f6efacae4758a3143cf44c7a84ba877
Rfinazzo4 <finazzo.ryan4@gmail.com>
2018-05-16 18:49:22 -0700

    git commit

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/storeData', function(req, res, next)
-{
-    res.render('index', { title: 'Express' });
-});
+//########################################
 
-module.exports = router;
+//to process data sent in on request need body-parser module
+
+var bodyParser = require('body-parser');
+var path = require ('path'); //to work with separtors on any OS including Windows
+var querystring = require('querystring'); //for use in GET Query string of form URI/path?name=value
+
+router.use(bodyParser.json()); // for parsing application/json
+router.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencode
+//#########################################
 
 //now processing post
 router.post('/storeData', function(req, res, next) {
-//expecting data variable called order--retrieve value using body-parser
-    var order = req.body.order;
-//retrieve the data associated with order
 
-    res.send("order succesfully received: " + order);
+    //expecting data variable called name --retrieve value using body-parser
+    var body = JSON.stringify(req.body);  //if wanted entire body as JSON
+    var params = JSON.stringify(req.params);//if wanted parameters
+    var cartorder = req.body.name;  //retrieve the data associated with name
+    res.send("Order succesfully recieved: " + cartorder);
 });
