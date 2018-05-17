5daa68db5df075facb83a22c64621721bd1bf387
Rfinazzo4 <finazzo.ryan4@gmail.com>
2018-05-17 08:49:12 -0700

    git commit

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var mongodb = require('mongodb');
+var mongoDBURI = process.env.MONGODB_URI || 'mongodb://Rfinazzo4:Rio-fizzo4@ds225840.mlab.com:25840/heroku_hnqdq9m4';
+
+//**************************************************************************
+//***** mongodb get all of the Routes in Routes collection where frequence>=1
+//      and sort by the name of the route.  Render information in the views/pages/mongodb.ejs
+router.get('/mongodb', function (request, response) {
+
+    mongodb.MongoClient.connect(mongoDBURI, function(err,  client) {
+        if(err) throw err;
+        //get handle to the database
+        var theDatabase = client.db('heroku_hnqdq9m4');
+
+
+        //get collection of Routes
+        var Routes = db.collection('Routes');
+        //get all Routes
+        Routes.find({ }).sort({ name: 1 }).toArray(function (err, docs) {
+            if(err) throw err;
+
+            response.render('/mongodb', {results: docs});
+
+        });
+
+        //close connection when your app is terminating.
+        db.close(function (err) {
+            if(err) throw err;
+        });
+    });//end of connect
+
+});//end XXX.get
 
 module.exports = router;
-
-//now processing post
-router.post('/storeData', function(req, res, next) {
-//expecting data variable called order--retrieve value using body-parser
-    var ordercart = req.body.order;
-//retrieve the data associated with order
-
-    res.send("order succesfully received: " + ordercart);
-});
