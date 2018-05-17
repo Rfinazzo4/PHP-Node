25569b3791d36c53f58b84dcf6bc083949efedf0
Rfinazzo4 <finazzo.ryan4@gmail.com>
2018-05-16 18:05:23 -0700

    git commit

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/storeData', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/storeData', function(req, res, next)
+{
+    res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
+
+//now processing post
+router.post('/storeData', function(req, res, next) {
+//expecting data variable called order--retrieve value using body-parser
+    var order = req.body.order;
+//retrieve the data associated with order
+
+    res.send("order succesfully received: " + order);
+});
