538a327f45e8b692ae14ef6580bcb790b33c30b5
Rfinazzo4 <finazzo.ryan4@gmail.com>
2018-05-24 09:34:29 -0700

    git commit

--- controllers/database.js
+++ controllers/database.js
@@ -1,20 +1,18 @@
-var express = require('express');
-var router = express.Router();
 
 var mongodb = require('mongodb');
 var mongoDBURI = process.env.MONGODB_URI || 'mongodb://Rfinazzo4:Rio-fizzo4@ds225840.mlab.com:25840/heroku_hnqdq9m4';
 
 //to process data sent in on request need body-parser module
-var bodyParser = require('body-parser');
-var path = require('path'); //to work with separtors on any OS including Windows
+//var bodyParser = require('body-parser');
+//var path = require('path'); //to work with separtors on any OS including Windows
 
-var querystring = require('querystring'); //for use in GET Query string of form URI/path?name=value
+//var querystring = require('querystring'); //for use in GET Query string of form URI/path?name=value
 
-router.use(bodyParser.json()); // for parsing application/json
-router.use(bodyParser.urlencoded({extended: true})); // for parsing application/x-www-form-urlencode
+//router.use(bodyParser.json()); // for parsing application/json
+//router.use(bodyParser.urlencoded({extended: true})); // for parsing application/x-www-form-urlencode
 
 
-module.exports.storeData = function (req, res, next) {
+module.exports.storeData = function (req, res) {
 
     /*
     var customer_info =[];
